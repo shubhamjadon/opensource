ae570c9cf3d12e05051ff743599130b21078e3af
Shubham Jadon <shubhamjadon99@gmail.com>
2019-10-04 18:24:33 +0530

    Merge sort also added

--- app.js
+++ app.js
@@ -65,3 +65,62 @@ int main(void) {
   }
   return 0;
 }
+
+//Merge Sort
+#include<stdio.h>
+void merge(int a[],int asize,int b[],int bsize,int arr[]){
+  int i=0,j=0,k=0;
+  while(i<asize && j<bsize){
+    if(a[i]<b[j]){
+      arr[k] = a[i];
+      i++;
+      k++;
+    }
+    else{
+      arr[k] = b[j];
+      j++;
+      k++;
+    }
+  }
+  while(i<asize){
+    arr[k]=a[i];
+    k++;i++;
+  }
+  while(j<bsize){
+    arr[k]=b[j];
+    k++;j++;
+  }
+}
+
+
+void breakit(int arr[],int size){
+  int a[size/2],b[size-size/2],i;
+  //assining values to new arrays
+  for(i=0;i<size/2;i++)
+    a[i] = arr[i];
+  for(i=0;i<size-size/2;i++)
+    b[i] = arr[i+size/2];
+  //using recursion to break arrays till we get single element array
+  if(sizeof(a)/4!=1)
+    breakit(a,sizeof(a)/4);
+  if(sizeof(b)/4!=1)
+    breakit(b,sizeof(b)/4);
+  //merging array when we reach stage when a&b are single element arrays
+  merge(a,sizeof(a)/4,b,sizeof(b)/4,arr);
+}
+
+
+int main(){
+  int arr[10] = {23,45,33,67,54,65,87,98,234,5},i;
+  printf("Array before sorting:\n");
+  for(int i=0;i<10;i++){
+    printf("%d ",arr[i]);
+  }
+  printf("\n");
+  breakit(arr,10);
+  printf("Array after sorting:\n");
+  for(int i=0;i<10;i++){
+    printf("%d ",arr[i]);
+  }
+  return 0;
+}
